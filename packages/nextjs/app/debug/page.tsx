908e51030af0fec2cca853a9c7c2d18dfde2cfa7
DevSoniaBatista <dev.soniabatista@gmail.com>
2024-05-10 16:51:57 -0300

    a

---
+++ packages/nextjs/app/debug/page.tsx
@@ -0,0 +1,21 @@
+import { DebugContracts } from "./_components/DebugContracts";
+import type { NextPage } from "next";
+import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
+
+export const metadata = getMetadata({
+  title: "Oly - Construindo o Futuro de Atletas Olimpicos",
+  description: "Construindo o Futuro de Atletas Olimpicos",
+});
+
+const Debug: NextPage = () => {
+  return (
+    <>
+      <DebugContracts />
+      <div className="text-center mt-8 bg-secondary p-10">
+      
+      </div>
+    </>
+  );
+};
+
+export default Debug;
